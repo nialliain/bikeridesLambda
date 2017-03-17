7691f4bedcd0a6b74a0a611f92d24e21774954b7
Niall <niall.i.campbell@gmail.com>
2017-03-17 12:58:24 +0000

    Build initial version.

---
+++ load.py
@@ -0,0 +1,84 @@
+import json
+import os
+from datetime import date
+
+import boto3
+import requests
+from googlemaps import Client
+from twilio.rest import TwilioRestClient
+
+s3 = boto3.resource('s3')
+
+
+def run_poll(event, context):
+    """ Update S3 with latest trackpoints from Spot. """
+    s3_history = get_history_from_s3()
+    s3_latest_timeStamp = s3_history[-1]['timeStamp']
+    spot_new = get_history_from_spot( s3_latest_timeStamp )
+    if spot_new:
+        combined_history = s3_history + spot_new
+        write_to_s3(combined_history)
+        notify_by_text(s3_history[-1], combined_history[-1])
+    else:
+        pass
+
+
+def latest_status():
+    """ Return a textual summary of the latest trackpoint. """
+    latest = get_history_from_s3()[-1]
+    return '{}\'s last tracked location was {} at {}.'.format(latest['name'], latest['location'], latest['timeStamp'])
+
+
+def is_newer_than(s3_latest_timeStamp, spot_message):
+    return s3_latest_timeStamp < str(spot_message['dateTime'])
+
+def get_history_from_spot(s3_latest_timeStamp):
+    gmaps = _get_gmaps()
+    def _build_track_point(spot_message):
+        def _reverse_geocode(lat, lon):
+            for addr in gmaps.reverse_geocode((lat, lon))[0]['address_components']:
+                if 'postal_town' in addr['types']:
+                   return addr['long_name']
+        track_point = dict()
+        track_point['lat'] = spot_message['latitude']
+        track_point['lon'] = spot_message['longitude']
+        track_point['timeStamp'] = str(spot_message['dateTime'])
+        track_point['messageType'] = str(spot_message['messageType'])
+        track_point['location'] = _reverse_geocode(spot_message['latitude'], spot_message['longitude'])
+        track_point['name'] = str(spot_message['messengerName'])
+        return track_point
+
+    r = requests.get(
+        'https://api.findmespot.com/spot-main-web/consumer/rest-api/2.0/public/feed/0kh77fpkuvgEaVFm0LklfeKXetFB6Iqgr/message.json')
+    track = [_build_track_point(msg) for msg in r.json()['response']['feedMessageResponse']['messages']['message'] if is_newer_than(s3_latest_timeStamp, msg)]
+    track.reverse()
+    return track
+
+
+def get_history_from_s3():
+    return json.loads(s3.Object('bikerid.es', 'track/history.json').get()['Body'].read())
+
+
+def write_to_s3(history):
+    def _write_file_to_s3( name, content ):
+        s3.Object('bikerid.es', 'track/{}.json'.format(name)).put(Body=json.dumps(content),
+                                                                  ContentType='application/json')
+    _write_file_to_s3('latest',history[-1])
+    _write_file_to_s3('history',history)
+
+
+def notify_by_text(previous_trackpoint, trackpoint):
+    if previous_trackpoint['timeStamp'].startswith(date.today().strftime('%Y-%m-%d')):
+        return
+    twilio = _get_twilio()
+    message = 'New location: {} at {}.'.format(trackpoint['location'], trackpoint['timeStamp'])
+    for number in ['+447793055904']:
+        twilio.messages.create(body=message, to=number, from_='+441631402022')
+
+
+def _get_twilio():
+    return TwilioRestClient(os.environ['twilio.account'], os.environ['twilio.token'])
+
+
+def _get_gmaps():
+    return Client(key=os.environ['gmaps.key'])
---
+++ test_load.py
@@ -0,0 +1,52 @@
+from unittest import TestCase
+from mock import Mock, patch
+from datetime import date
+import json
+
+from load import latest_status, is_newer_than, notify_by_text, get_history_from_spot
+
+
+class TestLoad(TestCase):
+
+
+    @patch('load.get_history_from_s3')
+    def test_latest_status(self, get_history_from_s3):
+        get_history_from_s3.return_value = [{'name': 'Niall',
+                                             'location': 'somewhere exotic',
+                                             'timeStamp': 'high noon'}]
+        status_text = latest_status()
+        self.assertEqual('Niall\'s last tracked location was somewhere exotic at high noon.', status_text)
+
+    def test_is_newer_than(self):
+        self.assertTrue(is_newer_than('2017-03-15T12:10:06+0000', {'dateTime':'2017-03-15T14:10:06+0000'}))
+        self.assertFalse(is_newer_than('2017-03-15T16:10:06+0000', {'dateTime':'2017-03-15T14:10:06+0000'}))
+
+    @patch('load._get_twilio')
+    def test_notify_by_text(self, _get_twilio):
+        mock_twilio = Mock()
+        _get_twilio.return_value = mock_twilio
+        notify_by_text({'timeStamp': date.today().strftime('%Y-%m-%d')}, {'timeStamp': date.today().strftime('%Y-%m-%d')})
+        mock_twilio.messages.create.assert_not_called()
+        notify_by_text({'timeStamp': '2017-03-15T12:10:06+0000'}, {'timeStamp': date.today().strftime('%Y-%m-%d'), 'location':'Slough'})
+        mock_twilio.messages.create.assert_called_with(body='New location: Slough at 2017-03-17.', from_='+441631402022', to='+447793055904')
+
+    @patch('load._get_gmaps')
+    @patch('requests.get')
+    def test_get_history_from_spot(self, get, get_gmaps):
+        sampleSpotResp = '''{"response":{"feedMessageResponse":{"count":2,"feed":{"id":"0kh77fpkuvgEaVFm0LklfeKXetFB6Iqgr","name":"poller","description":"poller","status":"ACTIVE","usage":0,"daysRange":7,"detailedMessageShown":true,"type":"SHARED_PAGE"},"totalCount":20,"activityCount":0,"messages":{"message":[{"@clientUnixTime":"0","id":709998376,"messengerId":"0-2533810","messengerName":"Niall","unixTime":1489587006,"messageType":"UNLIMITED-TRACK","latitude":56.44958,"longitude":-5.72681,"modelId":"SPOTTRACE","showCustomMsg":"Y","dateTime":"2017-03-15T14:10:06+0000","batteryState":"GOOD","hidden":0,"altitude":-103},{"@clientUnixTime":"0","id":709998429,"messengerId":"0-2533810","messengerName":"Niall","unixTime":1489586406,"messageType":"UNLIMITED-TRACK","latitude":56.44864,"longitude":-5.7378,"modelId":"SPOTTRACE","showCustomMsg":"Y","dateTime":"2017-03-15T14:00:06+0000","batteryState":"GOOD","hidden":0,"altitude":0}]}}}}'''
+        spotResp = Mock()
+        spotResp.json.return_value = json.loads(sampleSpotResp)
+        get.return_value = spotResp
+        gmaps = Mock()
+        gmaps.reverse_geocode.return_value = [{'address_components': []}]
+        get_gmaps.return_value = gmaps
+        resp = get_history_from_spot('2017-03-15T12:10:06+0000')
+        self.assertEqual(2, len(resp))
+        resp = get_history_from_spot('2017-03-15T14:00:06+0000')
+        self.assertEqual(1, len(resp))
+        print(resp)
+        self.assertEqual(resp[0]['name'], 'Niall')
+        self.assertEqual(resp[0]['timeStamp'], '2017-03-15T14:10:06+0000')
+        self.assertEqual(resp[0]['lat'], 56.44958)
+        self.assertEqual(resp[0]['lon'], -5.72681)
+        self.assertEqual(resp[0]['messageType'], 'UNLIMITED-TRACK')
